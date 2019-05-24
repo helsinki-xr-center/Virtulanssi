b5cff2892cfe664bee365b2f77bdd67941d11609
Emilie <emilie.lefrere@metropolia.fi>
2019-05-24 14:35:44 +0300

    Added 2 scripts and ambulance placeholder
    -Added script SteerinWheelInput (finished, allows the showing of normalized inputs in editor)
    -Added script AmbulanceController (unfinished)
    -Added beautiful ambulance placeholder in editor

---
+++ Virtulanssi/Assets/Scripts/AmbulanceController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbulanceController : MonoBehaviour
+{
+
+    public WheelCollider WheelColliderFL; // Front Left wheel collider
+    public WheelCollider WheelColliderFR; // Front Right wheel collider
+    public WheelCollider WheelColliderBL; // Back Left wheel collider
+    public WheelCollider WheelColliderBR; // Back Right wheel collider
+
+    public GameObject FL;
+    public GameObject FR;
+    public GameObject BL;
+    public GameObject BR;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
