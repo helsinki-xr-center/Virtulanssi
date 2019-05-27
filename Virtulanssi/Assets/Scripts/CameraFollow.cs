36e37f3fb52725da353e3de3878f8b19047b528c
Emilie <emilie.lefrere@metropolia.fi>
2019-05-27 13:33:58 +0300

    Fixes to WASD ambulance driving

---
+++ Virtulanssi/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    // Updates the camera position according to the target's position + an offset vector
+    void LateUpdate()
+    {
+        transform.position = target.position + offset;
+        transform.rotation = target.rotation;
+    }
+}
