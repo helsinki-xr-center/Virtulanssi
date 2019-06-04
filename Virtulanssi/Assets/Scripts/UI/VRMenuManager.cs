84178dcef26a2df50c549551f114d0d43c39e60d
Emilie <emilie.lefrere@metropolia.fi>
2019-06-04 16:33:58 +0300

    VR UI
    Copied normal UI to VR

---
+++ Virtulanssi/Assets/Scripts/UI/VRMenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VRMenuManager : MonoBehaviour
+{
+    public GameObject vrMainMenu;
+
+    LogitechGSDK.DIJOYSTATE2ENGINES rec;
+
+    // Activate main menu using escape. Also pauses/unpauses the game
+    private void Update()
+    {
+        rec = LogitechGSDK.LogiGetStateUnity(0);
+
+        if (rec.rgbButtons[6] == 128)
+        {
+            vrMainMenu.SetActive(!vrMainMenu.activeSelf);
+        }
+        if (vrMainMenu.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        if (!vrMainMenu.activeSelf)
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+}
