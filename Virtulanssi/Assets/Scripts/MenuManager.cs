0c10426b34bc09cd666cd2ec9df45d581162b8b5
Emilie <emilie.lefrere@metropolia.fi>
2019-06-04 15:33:42 +0300

    Camera improvement
    Can now scroll/rotate non-VR camera with mouse

--- Virtulanssi/Assets/Scripts/MenuManager.cs
+++ Virtulanssi/Assets/Scripts/MenuManager.cs
@@ -7,13 +7,21 @@ public class MenuManager : MonoBehaviour
 {
     public GameObject mainMenu;
 
+    // Activate main menu using escape. Also pauses/unpauses the game
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             mainMenu.SetActive(!mainMenu.activeSelf);
         }
-
+        if (mainMenu.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        if (!mainMenu.activeSelf)
+        {
+            Time.timeScale = 1f;
+        }
     }
 
 }
