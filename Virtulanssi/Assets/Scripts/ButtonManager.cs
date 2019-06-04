b407f371b76267328e7255490c518b0371975e33
Emilie <emilie.lefrere@metropolia.fi>
2019-06-04 12:05:26 +0300

    Non-VR menu
    Some non-VR UI functionality implemented, no pause game yet

---
+++ Virtulanssi/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void TestButton()
+    {
+        Debug.Log("Testing button...");
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Virtulanssi/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            mainMenu.SetActive(!mainMenu.activeSelf);
+        }
+
+    }
+
+}
