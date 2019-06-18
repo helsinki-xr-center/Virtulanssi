e519d7a53c92b5d9158c8d646d1496a59e2ebb4c
Emilie <emilie.lefrere@metropolia.fi>
2019-06-18 17:43:36 +0300

    Siren sound effect placeholder

---
+++ Virtulanssi/Assets/Scripts/Ambulance/Siren.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Siren : MonoBehaviour
+{
+    private bool isPlaying = false;
+    public AudioSource siren;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            isPlaying = !isPlaying;
+            if (isPlaying == true)
+            {
+                siren.Play();
+            }
+            else
+            {
+                siren.Pause();
+            }
+        }
+    }
+}
