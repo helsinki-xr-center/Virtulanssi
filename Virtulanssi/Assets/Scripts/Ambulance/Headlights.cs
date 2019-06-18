aadb75f3a819f23aaff67dbe5d1d26dd92199efa
Emilie <emilie.lefrere@metropolia.fi>
2019-06-18 17:09:23 +0300

    Headlights
    Head lights working, still need a texture

---
+++ Virtulanssi/Assets/Scripts/Ambulance/Headlights.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Headlights : MonoBehaviour
+{
+    public GameObject headlights;
+
+    public GameObject highBeamRight;
+    public GameObject highBeamLeft;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (!headlights.activeSelf)
+            {
+                headlights.SetActive(true);
+            }
+
+            else if (headlights.activeSelf)
+            {
+                headlights.SetActive(false);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (!highBeamRight.activeSelf)
+            {
+                highBeamRight.SetActive(true);
+                highBeamLeft.SetActive(true);
+            }
+            else if (highBeamRight.activeSelf)
+            {
+                highBeamRight.SetActive(false);
+                highBeamLeft.SetActive(false);
+            }
+
+        }
+    }
+}
